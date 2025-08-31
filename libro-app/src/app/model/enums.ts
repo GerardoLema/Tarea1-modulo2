0ca2cc621463f5b63c59a7e821be27615615c2f4
PC <segundol@tecnologicoismac.edu.ec>
2025-08-31 18:33:05 -0500

    Implementacion de Carrito  y CarritoItem  controller, dtos

---
+++ libro-app/src/app/model/enums.ts
@@ -0,0 +1,3 @@
+export type EstadoPedido = 'CREADO' | 'PAGADO' | 'EN_PREPARACION' | 'ENVIADO' | 'ENTREGADO' | 'CANCELADO';
+export type MetodoPago = 'TARJETA' | 'TRANSFERENCIA' | 'PAYPAL' | 'CONTRA_ENTREGA';
+
